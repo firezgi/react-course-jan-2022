7db9a201e1e22d2652eda800586ef40e19db2479
Habteab <keshi2017@gmail.com>
2022-01-10 16:25:11 -0800

    feat(templateLiteral):create temlateliteral

---
+++ es-review/templateLiterals.js
@@ -0,0 +1,10 @@
+const word1='Hab';
+const word2='Fire' ;
+console.log(word1)
+let fullName=`${word1} ${word2}`
+console.log(fullName)
+const sentense='Learn \n react.js'
+console.log("oldway: ",sentense)
+const nwsentense=`Learn 
+react.js`
+console.log("oldway: ",nwsentense)
